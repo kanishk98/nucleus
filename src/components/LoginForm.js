53186f6748dbeacb327898a6362bdb71ee6dbf0b
Kanishk Kakar <kanishk.kakar@gmail.com>
2018-07-27 00:32:55 +0530

    Verification added, re-attempt buggy

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {StyleSheet, View, Dimensions, Text, Platform, ProgressBarAndroid, ProgressViewIOS} from 'react-native';
 import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
 import renderIf from './renderIf';
+import {Auth} from 'aws-amplify';
 
 export default class LoginForm extends Component {
 
@@ -10,7 +11,8 @@ export default class LoginForm extends Component {
         this.state = {
             user: null,
             error: null,
-            progress: false
+            progress: false,
+            loggedIn: false
         };
     }
 
@@ -40,28 +42,30 @@ export default class LoginForm extends Component {
     }
 
     render() {
-        const {user} = this.state;
         const ProgressBar = Platform.select({
             ios: () => ProgressViewIOS,
             android: () => ProgressBarAndroid
         })();
-        if (!user) {
+        if (!this.state.loggedIn) {
             return (
-                <View style={styles.container}>
-                    {renderIf(!this.state.progress, <GoogleSigninButton
-                        style={styles.signInButton}
-                        size={GoogleSigninButton.Size.Icon}
-                        color={GoogleSigninButton.Color.Light}
-                        onPress={this.signIn}
-                    />)}
-                    {renderIf(this.state.progress, <ProgressBar/>)}
-                </View>
+            <View style={styles.container}>
+                <Text style={styles.instructions}>
+                    Sign in with your SNU account.
+                </Text>
+                {renderIf(!this.state.progress, <GoogleSigninButton
+                    style={styles.signInButton}
+                    size={GoogleSigninButton.Size.Icon}
+                    color={GoogleSigninButton.Color.Light}
+                    onPress={this.signIn}
+                />)}
+                {renderIf(this.state.progress, <ProgressBar/>)}
+            </View>
             );
         } else {
             return (
             <View style={styles.container}>
-                <Text>
-                    Welcome, {user.email}
+                <Text style={styles.instructions}>
+                    Signed in as {this.state.user.email}
                 </Text>
             </View>
             );
@@ -72,12 +76,18 @@ export default class LoginForm extends Component {
         try {
             this.setState({progress: true});
             const user = await GoogleSignin.signIn();
-            this.setState({user: user, error: null, progress: false})
+            console.log(user.email);
+            if (user.email.includes('@snu.edu.in')) {
+                console.log('Valid student');
+                this.setState({user: user, error: null, progress: false, loggedIn: true});
+            } else {
+                this.setState({user:null, error:null, progress:false, loggedIn:false});
+            }
         } catch (error) {
             if (error.code == 'CANCELED') {
                 error.message = 'User canceled login';
             }
-            this.setState({error});
+            this.setState({user:null, error:error, progress:false, loggedIn:false});
         }
     };
 }
@@ -92,4 +102,10 @@ const styles = StyleSheet.create({
         width: 312, 
         height: 60
     },
+    instructions: {
+       color: 'white',
+       marginBottom: 16,
+       fontSize: 18,
+       fontWeight: 'bold'
+    }
 });
