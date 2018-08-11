84f91c8899d1f8b47f6f00f19c71210991465373
Kanishk Kakar <kanishk.kakar@gmail.com>
2018-08-11 22:22:51 +0530

    Connected Apollo to React

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import LoginScreen from './src/components/LoginScreen';
 import RandomConnect from './src/components/RandomConnect';
 import React, {Component} from 'react';
 import SplashScreen from './src/components/SplashScreen'
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 const StackNavigator = createStackNavigator(
     {
@@ -30,8 +32,16 @@ const StackNavigator = createStackNavigator(
     }
 );
 
+const client = new ApolloClient({
+    uri: 'https://57v7t2mncraizj2efxdll47zqy.appsync-api.ap-south-1.amazonaws.com/graphql'
+});
+
 export default class App extends React.Component {
     render() {
-        return <StackNavigator />
+        return (
+            <ApolloProvider client={client}>
+                <StackNavigator />
+            </ApolloProvider>
+        );
     }
 }
---
+++ src/schema/AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://57v7t2mncraizj2efxdll47zqy.appsync-api.ap-south-1.amazonaws.com/graphql",
+    "region": "ap-south-1",
+    "authenticationType": "API_KEY",
+    "apiKey": "da2-wsns7jzggnhmtkqyhhlbq4ww6a"
+}
