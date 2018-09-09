7e39b6647d2a23d3a17e1b68ef2614eedb7f69c0
Kanishk Kakar <kanishk.kakar@gmail.com>
2018-09-10 01:00:52 +0530

    Converted index.js to latest Firebase API

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,28 @@
 functions = require('firebase-functions');
 admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
+var dryRun = true;
 exports.sendDiscoverTextNotification = functions.https.onRequest((req, res) => {
     const content = req.query.content;
     const recipient = req.query.recipient;
     const author = req.query.author;
+    const token = req.query.token;
     const payload = {
         notification: {
             title: "New message from " + {author},
-            body: {content} 
-        }
-    }
-    return admin.messaging().sendToDevice(recipient, payload);
+            body: {content},
+        },
+        android: {
+            priority: 'normal', 
+        },
+        token: {token},
+    };
+    admin.messaging().send(message, dryRun)
+    .then((response) => {
+        // response is a message ID
+        console.log(response);
+    })
+    .catch(error => {
+        console.log(error);
+    })
 })
