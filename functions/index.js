298e8b8c734854af3738724c56ae5d99ff50f959
kanishk98 <kanishk.kakar@gmail.com>
2018-09-09 23:36:15 +0530

    Function returning Google login page in Postman

--- functions/index.js
+++ functions/index.js
@@ -7,9 +7,12 @@ exports.sendDiscoverTextNotification = functions.https.onRequest((req, res) => {
     const author = req.query.author;
     const payload = {
         notification: {
-            title: "New message from " + {author},
-            body: {content} 
+            title: "New message from " + JSON.stringify({author}),
+            body: JSON.stringify({content}) 
         }
     }
     return admin.messaging().sendToDevice(recipient, payload);
 })
+
+// working request that returned a Google Sign-in page on Postman
+// https://us-central1-nucleus-2018.cloudfunctions.net/sendDiscoverTextNotification&recipient="Sarthak"&author="Kanishk"&content="Content"
