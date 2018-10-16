01ea71fba3d190bca9d4631ba8f4d16ac3b04eeb
Kanishk Kakar <kanishk.kakar@gmail.com>
2018-10-17 01:30:08 +0530

    Added POST requests in NewTrendingScreen

--- src/components/NewTrendingScreen.js
+++ src/components/NewTrendingScreen.js
@@ -1,27 +1,68 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Alert } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import Constants from '../Constants';
 
 
 export default class NewTrendingScreen extends React.Component {
+
+    caption = null;
+    option1 = null;
+    option2 = null;
    
-    _onSubmitEditing = () => {
-        console.log('editing done');
+    _onSubmitEditingCaption = ({text}) => {
+        console.log(text);
+        this.caption = text;
+    }
+
+    _onSubmitEditingOption1 = ({text}) => {
+        this.option1 = text;
+    }
+
+    _onSubmitEditingOption2 = ({text}) => {
+        this.option2 = text;
+    }
+
+    _submitPost = () => {
+        if (this.caption != null && this.option1 != null && this.option2 != null) {
+            fetch('http://' + Constants.postsIp + '/add-poll', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title: 'Anonymous',
+                    caption: this.caption,
+                    button1Title: this.option1,
+                    button2Title: this.option2,
+                }),
+            })
+            .then(res => {
+                Alert.alert('Post created', 'Yay! Your post was submitted.');
+            })
+            .catch(err => {
+                console.log(err);
+                Alert.alert('Oops', 'Something went wrong. Please check your network.');
+            });
+        } else {
+            Alert.alert('Missing fields', 'Please make sure to fill all fields before creating a post');
+        }
     }
 
     render () {
         return (
-            <View style={{justifyContent: 'center', backgroundColor: 'white'}}>
+            <View style={{justifyContent: 'center'}}>
                 <FormLabel>Caption</FormLabel>
-                <FormInput onSubmitEditing={this._onSubmitEditing}/>
+                <FormInput onChangeText={(text)=>this._onSubmitEditingCaption({text})}/>
                 <FormLabel>Option 1</FormLabel>
-                <FormInput onSubmitEditing={this._onSubmitEditing}/>
+                <FormInput onChangeText={(text)=>this._onSubmitEditingOption1({text})}/>
                 <FormLabel>Option 2</FormLabel>
-                <FormInput onSubmitEditing={this._onSubmitEditing}/>
+                <FormInput onChangeText={(text)=>this._onSubmitEditingOption2({text})}/>
                 <Button
                     style={{paddingTop: 20}}
                     title={'Post'}
-                    onPress={console.log('posted')}
+                    onPress={this._submitPost}
                 />
             </View>
         );
