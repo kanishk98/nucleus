cbffb115fe2542963ec0a3fbacef28e8ad6f7b18
Kanishk Kakar <kanishk.kakar@gmail.com>
2018-10-17 00:35:31 +0530

    Designed skeletal NewTrendingScreen

--- src/components/NewTrendingScreen.js
+++ src/components/NewTrendingScreen.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { View } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+
 
 export default class NewTrendingScreen extends React.Component {
+   
+    _onSubmitEditing = () => {
+        console.log('editing done');
+    }
+
     render () {
         return (
-            <View></View>
+            <View style={{justifyContent: 'center', backgroundColor: 'white'}}>
+                <FormLabel>Caption</FormLabel>
+                <FormInput onSubmitEditing={this._onSubmitEditing}/>
+                <FormLabel>Option 1</FormLabel>
+                <FormInput onSubmitEditing={this._onSubmitEditing}/>
+                <FormLabel>Option 2</FormLabel>
+                <FormInput onSubmitEditing={this._onSubmitEditing}/>
+                <Button
+                    style={{paddingTop: 20}}
+                    title={'Post'}
+                    onPress={console.log('posted')}
+                />
+            </View>
         );
     }
 }
