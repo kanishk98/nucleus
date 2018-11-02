f60ed7f8fc14e9d1b1b6d14b023c91a230ee814e
Kanishk Kakar <kanishk.kakar@gmail.com>
2018-11-02 18:05:03 +0530

    Add loading indicator to Polls

--- src/components/TrendingScreen.js
+++ src/components/TrendingScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NetInfo, StyleSheet, View, FlatList, AsyncStorage, ImageBackground, Dimensions } from 'react-native';
+import { NetInfo, StyleSheet, View, FlatList, ActivityIndicator, AsyncStorage, ImageBackground, Dimensions } from 'react-native';
 import { Card, Button } from 'react-native-elements';
 import Constants from '../Constants';
 import NavigationService from './NavigationService';
@@ -18,7 +18,7 @@ class PollCard extends React.PureComponent {
     updatePost = (button1Value, button2Value, userList) => {
         console.log('BUTTON1VAL: ' + this.state.button1Value);
         console.log('BUTTON2VAL: ' + this.state.button2Value);
-        fetch('http://' + Constants.postsIp + '/update-post',  {
+        fetch('http://' + Constants.postsIp + '/update-post', {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
@@ -35,47 +35,47 @@ class PollCard extends React.PureComponent {
                 userList: userList,
             }),
         })
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
     }
 
     onPressButton1 = () => {
         let userList = this.state.userList;
         userList[this.state.firebaseId] = true;
-        this.setState({button1Value: this.state.button1Value + 1, buttonPressed: true});
+        this.setState({ button1Value: this.state.button1Value + 1, buttonPressed: true });
         this.updatePost(this.state.button1Value + 1, this.state.button2Value, userList);
     }
 
     onPressButton2 = () => {
         let userList = this.state.userList;
         userList[this.state.firebaseId] = true;
-        this.setState({button2Value: this.state.button2Value + 1, buttonPressed: true});
+        this.setState({ button2Value: this.state.button2Value + 1, buttonPressed: true });
         this.updatePost(this.state.button1Value, this.state.button2Value + 1, userList);
     }
-    
+
     render() {
-        const {title, caption, image, button1Title, button2Title, button1Value, button2Value, userList, firebaseId} = this.state;
+        const { title, caption, image, button1Title, button2Title, button1Value, button2Value, userList, firebaseId } = this.state;
         return (
-            <Card containerStyle={{borderRadius: 15}}
+            <Card containerStyle={{ borderRadius: 15 }}
                 title={caption}>
-                    {renderSearch(
-                        this.state.buttonPressed || (!!userList && !!userList[firebaseId]),
-                        <View>
-                            <PollGraph label={button1Title} value={button1Value} total={button1Value + button2Value} />
-                            <View style={{paddingHorizontal: 30}} />
-                            <PollGraph label={button2Title} value={button2Value} total={button2Value + button1Value} />
-                        </View>,
-                        <View>
-                            <PollButton
-                                label={button1Title}
-                                onPress={this.onPressButton1}
-                            />
-                            <PollButton
-                                label={button2Title}
-                                onPress={this.onPressButton2}
-                            />
-                        </View>
-                    )}
+                {renderSearch(
+                    this.state.buttonPressed || (!!userList && !!userList[firebaseId]),
+                    <View>
+                        <PollGraph label={button1Title} value={button1Value} total={button1Value + button2Value} />
+                        <View style={{ paddingHorizontal: 30 }} />
+                        <PollGraph label={button2Title} value={button2Value} total={button2Value + button1Value} />
+                    </View>,
+                    <View>
+                        <PollButton
+                            label={button1Title}
+                            onPress={this.onPressButton1}
+                        />
+                        <PollButton
+                            label={button2Title}
+                            onPress={this.onPressButton2}
+                        />
+                    </View>
+                )}
             </Card>
         );
     }
@@ -90,7 +90,7 @@ export default class Trending extends React.PureComponent {
 
     constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             currentPage: 1,
             networkStatus: 'wifi',
         };
@@ -98,46 +98,46 @@ export default class Trending extends React.PureComponent {
     }
 
     getMorePosts = () => {
-        let {currentPage} = this.state;
+        let { currentPage } = this.state;
         currentPage = currentPage + 1;
         console.log('CurrentPage: ' + currentPage);
         fetch('http://' + Constants.postsIp + '/get-posts?perPage=5&currentPage=' + currentPage)
-        .then(res => {
-            console.log(res);
-            let tempList = this.state.postList;
-            console.log('TEMPLIST: ' + JSON.stringify(tempList));
-            let receivedList = JSON.parse(res._bodyInit);
-            console.log('RECEIVED LIST: ' + JSON.stringify(receivedList));
-            for (item in receivedList) {
-                if (tempList.indexOf(receivedList[item]) == -1) {
-                    tempList.push(receivedList[item]);
+            .then(res => {
+                console.log(res);
+                let tempList = this.state.postList;
+                console.log('TEMPLIST: ' + JSON.stringify(tempList));
+                let receivedList = JSON.parse(res._bodyInit);
+                console.log('RECEIVED LIST: ' + JSON.stringify(receivedList));
+                for (item in receivedList) {
+                    if (tempList.indexOf(receivedList[item]) == -1) {
+                        tempList.push(receivedList[item]);
+                    }
                 }
-            }
-            this.setState({postList: tempList, currentPage: currentPage});
-        })
-        .catch(err => {
-            console.log(err);
-        });
+                this.setState({ postList: tempList, currentPage: currentPage });
+            })
+            .catch(err => {
+                console.log(err);
+            });
     }
 
     getPosts = () => {
         fetch('http://' + Constants.postsIp + '/get-posts?perPage=5')
-        .then(res => {
-            console.log(res);
-            const postList = JSON.parse(res._bodyInit);
-            this.setState({postList: postList});
-        })
-        .catch(err => {
-            console.log(err);
-        });
+            .then(res => {
+                console.log(res);
+                const postList = JSON.parse(res._bodyInit);
+                this.setState({ postList: postList });
+            })
+            .catch(err => {
+                console.log(err);
+            });
     }
 
     postKeyExtractor = (item, index) => item._id;
 
-    renderPost = ({item}) => {
+    renderPost = ({ item }) => {
         return (
             <PollCard
-                _id = {item._id}
+                _id={item._id}
                 title={item.title || 'Anonymous'}
                 image={item.image}
                 caption={item.caption}
@@ -152,7 +152,7 @@ export default class Trending extends React.PureComponent {
     }
 
     _onrefresh = () => {
-        this.setState({refreshing: true});
+        this.setState({ refreshing: true });
         this.getPosts();
     }
 
@@ -164,8 +164,8 @@ export default class Trending extends React.PureComponent {
 
     handleConnectivityChange(connectionInfo) {
         console.log('First change, type: ' + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType);
-        this.setState({networkStatus: connectionInfo.type});
-      }
+        this.setState({ networkStatus: connectionInfo.type });
+    }
 
     componentWillUnmount() {
         NetInfo.removeEventListener(
@@ -175,7 +175,7 @@ export default class Trending extends React.PureComponent {
     }
 
     async componentDidMount() {
-        this.user = JSON.parse(await(AsyncStorage.getItem(Constants.UserObject)));
+        this.user = JSON.parse(await (AsyncStorage.getItem(Constants.UserObject)));
         NetInfo.getConnectionInfo().then((connectionInfo) => {
             console.log('Initial, type: ' + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType);
         });
@@ -194,16 +194,18 @@ export default class Trending extends React.PureComponent {
             this.getPosts();
             this.newTrendingSet = false;
         }
-        const {postList, networkStatus} = this.state;
-        if (networkStatus == 'none') {
+        const { postList, networkStatus } = this.state;
+        console.log()
+        if (!postList || postList.length == 0) {
             return (
-                <Card
-                    containerStyle={{backgroundColor: 'red'}}
-                    title={'You broke the Internet.'}
-                    titleStyle={{fontWeight: 'bold', color: 'white'}} 
-                />
+                <ImageBackground source={require('../../assets/background.png')} style={styles.backgroundStyle}>
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
+                        <ActivityIndicator />
+                    </View>
+                </ImageBackground>
             );
         }
+
         return (
             <ImageBackground source={require('../../assets/background.png')} style={styles.backgroundStyle}>
                 <FlatList
@@ -232,7 +234,7 @@ const styles = StyleSheet.create({
     backgroundStyle: {
         flex: 1,
         width: DEVICE_WIDTH,
-        height: DEVICE_HEIGHT, 
+        height: DEVICE_HEIGHT,
         backgroundColor: 'transparent'
     },
     resultText: {
@@ -240,8 +242,8 @@ const styles = StyleSheet.create({
     },
     pollButton: {
         borderRadius: 0,
-        marginLeft: 0, 
-        marginRight: 0, 
+        marginLeft: 0,
+        marginRight: 0,
         marginBottom: 0,
     },
 })
