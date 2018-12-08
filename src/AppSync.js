53fa947bac8cb4e508e4f9b16e2d48ee49d96947
kanishk98 <kanishk.kakar@gmail.com>
2018-12-08 17:46:08 +0530

    Clean slate

--- src/AppSync.js
+++
@@ -1,8 +0,0 @@
-﻿import Constants from "./Constants";
-
-export default {
-    "graphqlEndpoint": Constants.discoverEndpoint,
-    "region": "ap-south-1",
-    "authenticationType": "AWS_IAM",
-    "apiKey": "null"
-}
